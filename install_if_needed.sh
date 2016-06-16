4fd11745e15604596b6da60355e1fad57638312c
Dominic McGinnis <dominicmcginnis@gmail.com>
2016-06-16 09:29:34 -0700

    Update install_if_needed.sh
    Fixing issues with package special chars (@, ^, ~)

--- install_if_needed.sh
+++ install_if_needed.sh
@@ -4,17 +4,21 @@
 function install_package_if_needed() {
     local p=${1:-Package required}
     local v=${2:-Version required}
+    # Use this for version comps as it won't contain the special npm chars
+    local compV=$(echo $v | perl -pe 's/~//g; s/\^//g')
     if [[ ! -e "./node_modules/$p/package.json" ]]; then
 		npm install "$p@$v"
 	else
     	local i=$(node -p "require('$p/package.json').version")
-	    [ "$i" == "$v" ] || npm install "$p@$v"
+	    if [[ ! "$i" == "$compV" ]]; then
+	    	 npm install "$p@$v"
+	    fi
 	fi
 }
 
-declare -a modulesArray=$(node -e "var o = require('./package.json').devDependencies; for (var p in o) { console.log(p + '@' + o[p]); }")
+declare -a modulesArray=$(node -e "var o = require('./package.json').devDependencies; for (var p in o) { console.log(p + '_-_' + o[p]); }")
 
 for i in ${modulesArray[@]}; do
-        package=$(echo $i | perl -pe 's/@/ /g')
+        package=$(echo $i | perl -pe 's/_-_/ /g')
         install_package_if_needed ${package}
 done
